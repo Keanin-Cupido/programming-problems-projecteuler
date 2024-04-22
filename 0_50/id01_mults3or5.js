7a4abcea0cf8380799acc8ee9b04d58d20a0797a
Keanin Cupido <keanin16@gmail.com>
2024-04-22 11:38:45 +0200

    id01 problem solved

--- 0_50/id01_mults3or5.js
+++ 0_50/id01_mults3or5.js
@@ -1,1 +1,11 @@
-console.log('test');
+'use stict'
+
+let multiples = [];
+let sumMultiples = 0;
+
+for (let i = 0; i < 10; i++) {
+    multiples.push((i % 3 == 0) || (i % 5 == 0) ? i : null);
+}
+
+sumMultiples = multiples.reduce((sum, multiple) => sum + multiple, 0);
+console.log(sumMultiples);
