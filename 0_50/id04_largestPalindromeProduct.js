fe1c0fb63ae9335c22b223af745a1282cb79a420
Keanin Cupido <keanin16@gmail.com>
2024-05-06 15:12:17 +0200

    solved id04 palindrome problem

--- 0_50/id04_largestPalindromeProduct.js
+++ 0_50/id04_largestPalindromeProduct.js
@@ -1,3 +1,39 @@
 "use strict";
 
 // Finding the largest palindrome of the product of two three-digit numbers
+function findProduct(num1, num2) {
+    return num1 * num2;
+}
+
+// Check if the product is a palindrome
+function checkPalindrome(product) {
+    let productStr = String(product);
+    return (productStr === productStr.split('').reverse().join(''));
+}
+
+function findProducts(range) {
+    let palindromProducts = [];
+    for (let i = 1; i <= range; i++) {
+        for (let j = 1; j <= range; j++) {
+            let product = findProduct(i, j);
+            if (checkPalindrome(product)) {
+                palindromProducts.push(product);
+            }
+        }
+    }
+    return palindromProducts;
+}
+
+function findLargestPalindrome (range) {
+    const palindromProducts = findProducts(range);
+    let largestPalindrome = 0;
+    for (let i = 0; i < palindromProducts.length; i++) {
+        if (Number(palindromProducts[i]) > 0) {
+            largestPalindrome = palindromProducts[i];
+        }
+    }
+
+    return largestPalindrome;
+}
+
+console.log(findLargestPalindrome(1000));
